371777beceb3b2b8930ff85031284d3ec470e01a
Derek Banister <ptownderek@yahoo.com>
2021-09-24 18:54:56 -0700

    styling done, starting js next

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 //declare my questions and answers globally in one massive object
-var questionOne = [
+var questionContainer = [
     {
         question: "What type of event makes a button work?",
-        answers: {
-            a: 'clickOn',
-            b: 'doubleClick',
-            c: 'onClick',
-        },
-        correctAnswer: 'c'
+        answers: ["clickOn","doubleClick","onClick",],
+        correct: "onClick"
+           
     },
     {
         question: "What are the 3 main programming languages of the internet?",
-        answers: {
-            a: 'jQuery, MongoDB, Python',
-            b: 'HTML, CSS, JavaScript',
-            c: 'C++, CSharp, Unity',
-        },
-        correctAnswer: 'b'
+        answers: ["jQuery, MongoDB, Python","HTML, CSS, JavaScript","C++, CSharp, Unity"],
+        correct: "HTML, CSS, JavaScript",    
+  
     },
     {
         question: "What is one way you can make a website mobile-friendly?",
-        answers: {
-            a: 'Media Queries',
-            b: 'Smaller font sizes',
-            c: 'You dont mobile users do not matter',
-        },
-        correctAnswer: 'a'
+        answers: ["Media Queries","Smaller font sizes", "You dont mobile users do not matter"],
+        correctAnswer: "Media Queries"   
     },
 
 ]
+var questionContainer = document.getElementById("question");
+var containerMain = document.getElementById("mainContainer");
+var startButton = document.getElementById("quizStart")
 
-//I am going to need a few main functions, one that displays the question
+document.getElementById("quizStart").addEventListener("click", displayQuestion);
+
+//I am going to need a few main functions, one that displays the question and gets
+//rid of start button
+function displayQuestion() {
+   
+}
+
+
+
+
 //and answers, one that shows the results at the end, one that handles the timer,
 //and one that stores the data and adds to scoreboard.
