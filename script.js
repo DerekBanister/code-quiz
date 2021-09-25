bb097e2c77bd7161b27c7333a4ef732f884b193b
Derek Banister <ptownderek@yahoo.com>
2021-09-25 10:50:48 -0700

    Button hidden onClick and questions/choices displaying and formatted

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //declare my questions and answers globally in one massive object
 var questionArray = [
     {
-        question: "What type of event makes a button work?",
+        question: "What type of event in JavaScript makes a button work?",
         answers: ["clickOn","doubleClick","onClick",],
         correct: "onClick"
            
@@ -24,35 +24,37 @@ var scoreboard = 0;
 var questionAmount = 0;
 var timerBox = document.querySelector("#timer");
 var questionBox = document.querySelector("#questions");
-var choicesContainer = document.querySelector("#choices");
 var containerMain = document.querySelector("#mainContainer");
 var startButton = document.querySelector("#quizStart");
 var createUl = document.createElement("ul");
 
-//variables for timer?
+//variables for timer
 var totalSeconds = 60;
 var holdPenaltyTime= 0;
 var penaltyTime = 10;
 var createList = document.createElement("ul");
 
  
-//starts timer and tracks penalty times.
+//starts timer and tracks penalty times. Calling displayQuestion.
 quizStart.addEventListener("click", function(){
+   
+   //BUTTON HIDDEN!
+    this.style.visibility = "hidden";
     //check if timer at zero
     if (holdPenaltyTime === 0){
         holdPenaltyTime = setInterval(function(){
             totalSeconds--;
-            timerBox.textContent = "Time: " + totalSeconds;
+            timerBox.textContent = "Time left: " + totalSeconds;
     
     if (totalSeconds === 0){
         clearInterval(holdPenaltyTime);
-        timerBox.textContent = "Time's up!";
+        timerBox.textContent = "Time is up!";
 
         }
     }, 1000);
 }
    //TIMER WORKING!!!
-console.log(quizStart, timerBox)
+//console.log(quizStart, timerBox)
 displayQuestion(questionAmount);
 } 
 
@@ -68,30 +70,23 @@ displayQuestion(questionAmount);
             var gameAnswers = questionArray[questionAmount].answers;
             questionBox.textContent = gameQuestion;
 
-            console.log(questionContainer);
+            console.log(questionArray);
         }
-        //select one question
+        //select one question and create list
             gameAnswers.forEach(function (newQuestion){
                 var listQuestion = document.createElement("li");
                 listQuestion.textContent = newQuestion;
                 questionBox.appendChild(createList);
                 createList.appendChild(listQuestion);
-            }
-            )
-        //question is displaying atleast, need to figure out answers and to 
-        //hide button.
+         }
+     )
+}
+//everything displaying as intended. Next is to figure out how to go to
+//next question and create if statements for correct answers.
 
 
 
- }
-
-
-
-
-//Something that compares question choice with answer
-
-
-
+ 
 
 
 //Function that stores the data and adds to scoreboard.
